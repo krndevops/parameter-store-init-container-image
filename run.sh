6ee0bdea54e8541d6554c0f87e7b6abecfaecbcc
metal <metalheadkrn@gmail.com>
2026-04-03 17:53:22 +0530

    Parameters support

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-set -x
+
 for param in $PARAMS ; do
-  aws ssm get-parameter --name $param --region us-east-1
+  PARAM=$(echo $param | awk -F , '{print $1}')
+  KEY=$(echo $param | awk -F , '{print $2}')
+  VALUE=aws ssm get-parameter --name $param--region us-east-1 --with-decryption --query "Value" --output text
+  echo "export $KEY=\"$VALUE\"" >>/parameters/params
 done
